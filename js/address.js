e48e28bf34a67b215aa89e717d6f2a2a9d8df087
Ersoy Efe Uruk <efeuruk1@gmail.com>
2018-12-19 00:05:57 +0300

    Some fixes on payment form

--- js/address.js
+++ js/address.js
@@ -9,11 +9,10 @@ s_a[2]="Famagusta|Kyrenia|Larnaca|Limassol|Nicosia|Paphos";
 s_a[3]="Baden-Wuerttemberg|Bayern|Berlin|Brandenburg|Bremen|Hamburg|Hessen|Mecklenburg-Vorpommern|Niedersachsen|Nordrhein-Westfalen|Rheinland-Pfalz|Saarland|Sachsen|Sachsen-Anhalt|Schleswig-Holstein|Thueringen";
 s_a[4]="Abseron Rayonu|Agcabadi Rayonu|Agdam Rayonu|Agdas Rayonu|Agstafa Rayonu|Agsu Rayonu|Ali Bayramli Sahari|Astara Rayonu|Baki Sahari|Balakan Rayonu|Barda Rayonu|Beylaqan Rayonu|Bilasuvar Rayonu|Cabrayil Rayonu|Calilabad Rayonu|Daskasan Rayonu|Davaci Rayonu|Fuzuli Rayonu|Gadabay Rayonu|Ganca Sahari|Goranboy Rayonu|Goycay Rayonu|Haciqabul Rayonu|Imisli Rayonu|Ismayilli Rayonu|Kalbacar Rayonu|Kurdamir Rayonu|Lacin Rayonu|Lankaran Rayonu|Lankaran Sahari|Lerik Rayonu|Masalli Rayonu|Mingacevir Sahari|Naftalan Sahari|Naxcivan Muxtar Respublikasi|Neftcala Rayonu|Oguz Rayonu|Qabala Rayonu|Qax Rayonu|Qazax Rayonu|Qobustan Rayonu|Quba Rayonu|Qubadli Rayonu|Qusar Rayonu|Saatli Rayonu|Sabirabad Rayonu|Saki Rayonu|Saki Sahari|Salyan Rayonu|Samaxi Rayonu|Samkir Rayonu|Samux Rayonu|Siyazan Rayonu|Sumqayit Sahari|Susa Rayonu|Susa Sahari|Tartar Rayonu|Tovuz Rayonu|Ucar Rayonu|Xacmaz Rayonu|Xankandi Sahari|Xanlar Rayonu|Xizi Rayonu|Xocali Rayonu|Xocavand Rayonu|Yardimli Rayonu|Yevlax Rayonu|Yevlax Sahari|Zangilan Rayonu|Zaqatala Rayonu|Zardab Rayonu";
 
-function populateStates( countryElementId, stateElementId ){
+function populateStates(countryElementId, stateElementId) {
 	
-	let selectedCountryIndex = document.getElementById( countryElementId ).selectedIndex;
-
-	let stateElement = document.getElementById( stateElementId );
+	let selectedCountryIndex = document.getElementById(countryElementId).selectedIndex;
+	let stateElement = document.getElementById(stateElementId);
 	
 	stateElement.length=0;	
 	stateElement.options[0] = new Option('Select State','');
@@ -26,7 +25,7 @@ function populateStates( countryElementId, stateElementId ){
 	}
 }
 
-function populateCountries(countryElementId, stateElementId){
+function populateCountries(countryElementId, stateElementId) {
 	// given the id of the <select> tag as function argument, it inserts <option> tags
 	let countryElement = document.getElementById(countryElementId);
 	countryElement.length=0;
@@ -38,9 +37,53 @@ function populateCountries(countryElementId, stateElementId){
 
 	// Assigned all countries. Now assign event listener for the states.
 
-	if( stateElementId ){
+	if(stateElementId){
 		countryElement.onchange = function(){
 			populateStates( countryElementId, stateElementId );
 		};
 	}
 }
+
+function generateMonthsAndYears() {
+	document.getElementById("months").options[0] = new Option("");
+	document.getElementById("years").options[0] = new Option("");
+	for(i=0; i<12; i++) {
+		document.getElementById("months").options[i+1] = new Option(i+1);
+	}
+	for (i=0; i<5; i++) {
+		document.getElementById("years").options[i+1] = new Option(i+2018);
+	}
+}
+
+function preventNumbers() {
+    $('.onlyText').keydown(function (e) {
+        if (/*e.shiftKey || */ e.ctrlKey || e.altKey) {
+            e.preventDefault();
+        }
+        else {
+            let key = e.keyCode;
+            if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90) || (key == 222))) {
+                e.preventDefault();
+            }
+        }
+    });
+}
+
+// Numara isteyen yerlere text girilmesini engellemek
+function preventTexts() {
+    $('.onlyNumber').keydown(function (e) {
+        if (e.shiftKey || e.ctrlKey || e.altKey) {
+            e.preventDefault();
+        }
+        else {
+            var key = e.keyCode;
+            if (((key == 32) || (key == 46) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90))) {
+                e.preventDefault();
+            }
+        }
+    });
+}
+
+$(document).ready(function() {
+	generateMonthsAndYears();preventNumbers(); preventTexts();
+});
